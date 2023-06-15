056acaedcdf0242127b5239107fe818df856342c
anjankarmakar10 <anjankarmakar15@gmail.com>
2023-06-15 11:44:06 +0600

    display genre

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,22 @@ function App() {
         base: `"nav""main"`,
         lg: `"nav nav""aside main"`,
       }}
+      templateColumns={{
+        base: "1fr",
+        lg: "200px 1fr",
+      }}
     >
       <GridItem area={"nav"}>
         <Navbar />
       </GridItem>
       <Show above="lg">
-        <GridItem area={"aside"}>
+        <GridItem
+          height={"fit-content"}
+          position={"sticky"}
+          top={0}
+          area={"aside"}
+          paddingX={5}
+        >
           <GenreList />
         </GridItem>
       </Show>
